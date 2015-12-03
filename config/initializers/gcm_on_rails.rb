96c2ebb07059aff198702cf2e7bd6fe81ad38c01
robin.guillard <robin.guillard@gmail.com>
2015-12-03 16:48:58 +0100

    'smileback'

--- config/initializers/gcm_on_rails.rb
+++ config/initializers/gcm_on_rails.rb
@@ -1,4 +1,4 @@
 configatron.gcm_on_rails.api_url = 'https://android.googleapis.com/gcm/send'
 configatron.gcm_on_rails.api_key = 'AIzaSyAGlz6b-YleHYP7bOz1dH7fhFTk-4UPfwY'
-configatron.gcm_on_rails.app_name = 'com.wakeupsunshine-1145.com'
+configatron.gcm_on_rails.app_name = 'com.tranquil-inlet-2994.com'
 configatron.gcm_on_rails.delivery_format = 'json'
